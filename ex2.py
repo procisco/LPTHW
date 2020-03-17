aa8c11ba0bfb9add2cba9095bfc76996667a49cb
John Knoernschild <procisco@gmail.com>
2020-03-17 17:04:07 -0400

    added 3rd line to ex2.py

--- ex2.py
+++ ex2.py
@@ -1,5 +1,6 @@
 # A comment, this is so you can read your program later.
 # Anything after the # is ignored by python.
+# I can break this merge
 
 print("I could have code like this.") # and the comment after is ignored
 
