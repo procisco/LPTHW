b3805d2ba2ee85ea23b0a9eb355fbf35abc7cd31
John Knoernschild <procisco@gmail.com>
2020-03-17 16:47:51 -0400

    new file ex2.py

---
+++ ex2.py
@@ -0,0 +1,17 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+print("Okay, not much happening here")
+color = input("Enter either 'red' or 'blue': ")
+
+if color == 'red':
+    print("Red is my favorite color")
+else:
+    print('#' * 30)
+    print("Blue is the color of budgies")
