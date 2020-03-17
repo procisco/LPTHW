1ee68d157e234d5717fc4f11151e900ce0941b8b
John Knoernschild <procisco@gmail.com>
2020-03-17 16:17:52 -0400

    added a loop to ex1.py

--- ex1.py
+++ ex1.py
@@ -5,3 +5,8 @@ print("Yay! Printing.")
 #print("I'd much rather you 'not'.")
 #print('I "said" do not touch this.')
 #print('yet another line')
+# create a list
+alphabit = [a, b, c, d, e, f, g, h, i, j, k]
+for letter in alphabit:
+    print(letter)
+
