f7516d87715d27e5aa41afa322532854920b8e9b
John Knoernschild <procisco@gmail.com>
2020-03-18 20:45:33 -0400

    deleted unit test1.py

--- unit test1.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestParse(unittest.TestCase):
-    def test_parse_cust_name(self):
-        cp = ConfigurationParser()
-        expected_names = ['CUSTOMER A', 'CUSTOMER B']
-        parsed_names = cp.parseCustomerNames()
-        self.assertEqual(list, type(parsed_names))
-        self.assertEqual(expected_names, parsed_names)
